929f4453a28bb992fe4215addd6fdc13df6de6ed
Dennis-Pham-creator <ddupham@ucdavis.edu>
2025-03-08 04:02:09 -0800

    Added starter code for worker.py
    Please read over this basic code and make changes as you see fit

--- lstore/transaction_worker.py
+++ lstore/transaction_worker.py
@@ -1,44 +1,51 @@
+from threading import Thread
 from lstore.table import Table, Record
 from lstore.index import Index
 
 class TransactionWorker:
+    """
+    Manage and execute multiple transactions concurrently using threads
+    """
 
-    """
-    # Creates a transaction worker object.
-    """
-    def __init__(self, transactions = []):
-        self.stats = []
-        self.transactions = transactions
-        self.result = 0
-        pass
+    def __init__(self, transactions=[]):
+        """
+        Creates a TransactionWorker object
 
-    
-    """
-    Appends t to transactions
-    """
+        transactions (list): A list of transactions to be executed by this worker
+        """
+        self.stats = []  # List to store  result of each transaction (True if committed, False if aborted)
+        self.transactions = transactions  # List of transactions to be executed
+        self.result = 0  # Number of transactions that committed successfully
+        self.thread = None  # Thread object for running transactions concurrently
+
     def add_transaction(self, t):
+        """
+        Adds a transaction to list of transactions to be executed
+
+        t is the transaction to be added
+        """
         self.transactions.append(t)
 
-        
-    """
-    Runs all transaction as a thread
-    """
     def run(self):
-        pass
-        # here you need to create a thread and call __run
-    
+        """
+        Starts execution of all transactions in a separate thread
+        """
+        self.thread = Thread(target=self.__run)  # Create a thread to run transactions
+        self.thread.start()  
 
-    """
-    Waits for the worker to finish
-    """
     def join(self):
-        pass
-
+        """
+        Waits for worker thread to finish execution
+        """
+        if self.thread:
+            self.thread.join()  # Wait for thread to complete
 
     def __run(self):
+        """
+        Private method to execute all transactions and record their results
+        """
         for transaction in self.transactions:
-            # each transaction returns True if committed or False if aborted
+            # Execute each transaction and record whether it committed or aborted
             self.stats.append(transaction.run())
-        # stores the number of transactions that committed
+        # Calculate number of transactions that committed successfully
         self.result = len(list(filter(lambda x: x, self.stats)))
-
